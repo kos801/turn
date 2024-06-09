9259fd75a455fc5800475fc6f0276be041a38060
kos801 <xn83bs@outlook.com>
2024-06-09 15:34:53 +0800

    Modify since.cpp

--- since.cpp
+++ since.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Million scene ago arm parent keep.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
