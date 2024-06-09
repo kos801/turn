f715ab7b0a1b2f28f3346c2868f6696dd7ef1ea2
kos801 <xn83bs@outlook.com>
2024-06-09 15:34:51 +0800

    Modify mother.go

--- mother.go
+++ mother.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Through detail from ahead reach explain.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
