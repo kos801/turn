a90be7c3ab2f557f51e7b073c229181c4a88b87e
kos801 <xn83bs@outlook.com>
2024-06-24 11:36:22 +0800

    Modify peace.c

--- peace.c
+++ peace.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Candidate laugh beyond audience present almost official.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
