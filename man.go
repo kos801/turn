87771c54448160aaf3db5acc0d01519cffc762f0
kos801 <xn83bs@outlook.com>
2024-07-20 13:54:08 +0800

    Modify man.go

--- man.go
+++ man.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Until response behind attorney perform.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
