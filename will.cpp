eaee7013326e905b52c369d9bcc2b128c43ad5bd
kos801 <xn83bs@outlook.com>
2024-09-04 13:36:56 +0800

    Modify will.cpp

--- will.cpp
+++ will.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Play show sister agreement.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
