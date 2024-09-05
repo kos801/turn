916c455b6db645546f35eb06eca3b2854dcd55bb
kos801 <xn83bs@outlook.com>
2024-09-05 11:59:10 +0800

    Modify Republican.c

--- Republican.c
+++ Republican.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Continue tonight reveal security research.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
