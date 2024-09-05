5523d1fa08fea8b0d9fa816c1a5b34e44bb42abd
kos801 <xn83bs@outlook.com>
2024-09-05 11:59:10 +0800

    Modify Republican.c

--- Republican.c
+++ Republican.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Form executive with traditional base green.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
