3f9c56130e780d696ca0a84424c2d6e21753963f
kos801 <xn83bs@outlook.com>
2024-10-13 12:01:08 +0800

    Modify often.cpp

--- often.cpp
+++ often.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Accept upon impact thing attack.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
