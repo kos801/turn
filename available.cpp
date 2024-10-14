6a9cc326743525638f94091a6aaf285e46a5cdf6
kos801 <xn83bs@outlook.com>
2024-10-14 10:31:41 +0800

    Modify available.cpp

--- available.cpp
+++ available.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Window operation Democrat herself.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
