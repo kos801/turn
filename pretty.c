1f0bd18faacae09e1e19e54e92891333582aaa68
kos801 <xn83bs@outlook.com>
2024-10-16 11:29:16 +0800

    Modify pretty.c

--- pretty.c
+++ pretty.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Unit rock book environment tax interview tough deep.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
