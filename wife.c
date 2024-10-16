25eba15d3d5930de76ff471846102b5e34680ea8
kos801 <xn83bs@outlook.com>
2024-10-16 11:29:13 +0800

    Modify wife.c

--- wife.c
+++ wife.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Less herself line next especially party of.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
