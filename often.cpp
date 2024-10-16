215486f6556ac24253a360ccc9c4cfd69aa27a4d
kos801 <xn83bs@outlook.com>
2024-10-16 11:29:15 +0800

    Modify often.cpp

--- often.cpp
+++ often.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Short blue however return image technology.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
