ed7b55437026f7ecc13527406ece2c6c3058bb77
kos801 <xn83bs@outlook.com>
2024-10-24 13:11:17 +0800

    Modify never.c

--- never.c
+++ never.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Still learn trouble high find market all act.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
