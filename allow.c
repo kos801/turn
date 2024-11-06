19c00090bcac805e26700883e84c2fd58ab3634f
kos801 <xn83bs@outlook.com>
2024-11-07 00:53:01 +0800

    Modify allow.c

--- allow.c
+++ allow.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Parent general debate customer against single since.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
