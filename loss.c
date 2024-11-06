63643f5247abc1603558187dc242f89d53de55a9
kos801 <xn83bs@outlook.com>
2024-11-07 00:53:01 +0800

    Modify loss.c

--- loss.c
+++ loss.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Near institution executive add national.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
