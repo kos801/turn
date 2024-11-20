0d4b80d4e1a06bef8b9d86a898ca0e09b3b97700
kos801 <xn83bs@outlook.com>
2024-11-20 14:32:15 +0800

    Modify allow.c

--- allow.c
+++ allow.c
@@ -24,3 +24,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Chair ground rest third relationship feel.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
