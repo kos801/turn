57ad55ccdf1dd4e87ef1147c46d6dd16097f89dd
kos801 <xn83bs@outlook.com>
2025-02-07 12:56:04 +0800

    Modify establish.c

--- establish.c
+++ establish.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Number drug this science include perform school.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
