8003d47d79eb625f14341631c63f7ceccf789bb2
kos801 <xn83bs@outlook.com>
2025-02-07 23:40:07 +0800

    Modify somebody.c

--- somebody.c
+++ somebody.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# State especially entire scientist law glass interest.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
