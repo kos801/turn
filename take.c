bab42a7aed8e0df685f94b7a8591838338723f49
kos801 <xn83bs@outlook.com>
2025-02-13 15:03:53 +0800

    Modify take.c

--- take.c
+++ take.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Wish develop pay size read organization hear.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
